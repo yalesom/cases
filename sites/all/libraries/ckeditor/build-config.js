d0c1fd3e4e8b3ae5363fc73e9c62b0a5321a6555
George Heimel <george.heimel@square360.com>
2014-08-13 11:00:48 -0400

    Style changes for pages
    Signed-off-by: George Heimel <george.heimel@square360.com>

--- sites/all/libraries/ckeditor/build-config.js
+++ sites/all/libraries/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/4dfa969449d67d66a091b5da35b73a0d
+ * (2) http://ckeditor.com/builder/beb6c1d59cf43488ac9cde1fcc163f60
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/4dfa969449d67d66a091b5da35b73a0d
+ * (3) http://ckeditor.com/builder/download/beb6c1d59cf43488ac9cde1fcc163f60
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -25,8 +25,8 @@
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'moonocolor',
-	preset: 'full',
+	skin: 'moono',
+	preset: 'standard',
 	ignore: [
 		'.bender',
 		'.DS_Store',
@@ -47,56 +47,33 @@ var CKBUILDER_CONFIG = {
 		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
-		'bidi' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
 		'contextmenu' : 1,
-		'dialogadvtab' : 1,
-		'div' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'filebrowser' : 1,
-		'find' : 1,
-		'flash' : 1,
-		'floating-tools' : 1,
 		'floatingspace' : 1,
-		'font' : 1,
 		'format' : 1,
-		'forms' : 1,
 		'gg' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
-		'iframe' : 1,
 		'image' : 1,
 		'image2' : 1,
-		'indentblock' : 1,
 		'indentlist' : 1,
-		'justify' : 1,
-		'language' : 1,
 		'link' : 1,
 		'list' : 1,
-		'liststyle' : 1,
 		'magicline' : 1,
 		'maximize' : 1,
 		'mediaembed' : 1,
-		'newpage' : 1,
-		'pagebreak' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
-		'preview' : 1,
-		'print' : 1,
 		'quicktable' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
-		'save' : 1,
 		'scayt' : 1,
-		'selectall' : 1,
-		'showblocks' : 1,
 		'showborders' : 1,
-		'smiley' : 1,
 		'sourcearea' : 1,
 		'sourcedialog' : 1,
 		'specialchar' : 1,
@@ -105,21 +82,12 @@ var CKBUILDER_CONFIG = {
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
-		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
 		'wsc' : 1,
 		'wysiwygarea' : 1
 	},
 	languages : {
-		'de' : 1,
-		'en' : 1,
-		'es' : 1,
-		'fr' : 1,
-		'fr-ca' : 1,
-		'it' : 1,
-		'pt' : 1,
-		'pt-br' : 1,
-		'zh-cn' : 1
+		'en' : 1
 	}
 };
