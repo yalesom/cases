1d513da07490f70996ec2678603d2000020bf9d3
George Heimel <george.heimel@square360.com>
2014-11-21 16:13:50 -0500

    premium backport CSS

--- sites/all/themes/ysm_cases/_assets/gulpfile.js
+++ sites/all/themes/ysm_cases/_assets/gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('styles',function() {
 		.pipe(compass({
 			project: path.join(__dirname,"."),
 			//config_file: 'config.rb',
-			css: 'temp',
+			css: 'css',
 			sass: 'scss',
 			comments: function(){
 				return (pkg.environment == 'production') ? false : true;
