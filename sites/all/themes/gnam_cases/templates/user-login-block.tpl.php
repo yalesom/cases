e14f5dc26392347adfd77ac1d0a5f756b5d68f34
Brian Milea <brian.milea@square360.com>
2014-11-26 15:35:25 -0500

    Add protocol to login link

--- sites/all/themes/gnam_cases/templates/user-login-block.tpl.php
+++ sites/all/themes/gnam_cases/templates/user-login-block.tpl.php
@@ -6,10 +6,11 @@
     <?php print $rendered; // Display hidden elements (required for successful login) ?> 
   <?php 
 	$url = $_SERVER['HTTP_HOST'];
+	$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
 	if (arg(0) == 'node' && is_numeric(arg(1))) { 
 		$nodeid = arg(1); 
 		print '<p>Yale students and faculty may also login with their NetID.</p>';
-		print l('Login with NetID','https://secure.its.yale.edu/cas/login?service='.$url.'%2Fcas%3Fdestination%3Dnode%2F'.$nodeid); 
+		print l('Login with NetID','https://secure.its.yale.edu/cas/login?service='.$protocol.$url.'%2Fcas%3Fdestination%3Dnode%2F'.$nodeid); 
 	};
 	?>
   </div>
