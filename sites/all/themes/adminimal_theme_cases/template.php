4f5c0cf31b0c0a8963ae1acdee900567bb99dd13
George Heimel <george.heimel@square360.com>
2017-05-12 17:04:53 -0400

    theme template file fixes for admin
    CASES-205

--- sites/all/themes/adminimal_theme_cases/template.php
+++ sites/all/themes/adminimal_theme_cases/template.php
@@ -1,8 +1,8 @@
 <?php
 
-function adminimal_theme_cases_preprocess_html(&$variables) {
+function adminimal_cases_preprocess_html(&$variables) {
   drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array('type' => 'external'));
-  drupal_add_css($adminimal_path . 'assets/distribution/css/style-overrides.app.min.css', array('group' => CSS_THEME, 'media' => 'all', 'weight' => 100));
+  drupal_add_css(path_to_theme() . 'assets/distribution/css/style-overrides.app.min.css', array('group' => CSS_THEME, 'media' => 'all', 'weight' => 100));
 }
 
 ?>
