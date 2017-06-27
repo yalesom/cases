d1ff939c9cdb712d62bee275e1c510c024bd0c5d
Carlos Coto <cc2473@SOM-DKQR07LF9VN.som.yale.internal>
2017-06-27 13:22:42 -0400

    Changed to put in SEO tags for bookshelp images

--- sites/all/themes/gnam_cases/templates/semanticviews-view-fields--document-grid--document-grid.tpl.php
+++ sites/all/themes/gnam_cases/templates/semanticviews-view-fields--document-grid--document-grid.tpl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $color 		= ($fields['field_color']->content == '') ? 'yale' : $fields['field_color']->content;
 $headline 	= $fields['field_headline']->content;
 $icon 		= $fields['field_icon']->content;
@@ -7,6 +7,7 @@ $pageURL 	= $fields['field_page_url']->content;
 $href		= ($document != '') ? $document : $pageURL;
 $teaser 	= $fields['field_teaser']->content;
 $image 		= $fields['field_image']->content;
+$seo_image = "$image alt=" . '"' . $headline . '"' . "title=$teaser";
 $isInline 	= $fields['field_inline_show']->content;
 $inlineHTML = $fields['field_inline_html']->content;
 ?>
@@ -17,14 +18,9 @@ $inlineHTML = $fields['field_inline_html']->content;
 		<div class="tile-wrap <?php print $color ?>">
 			<a href="<?php print $href ?>" target="_blank">
 			<span class="img">
-				<img src="<?php print $image ?>" />
+				<img src="<?php print $seo_image ?>" />
 				<i class="<?php print $icon ?>"></i>
 			</span>
-			
-			<span class="text">
-				<span class="header zeta"><?php print $headline ?></span>
-			</span>
-			<span class="tooltip"><span class="tooltext"><?php print $teaser ?></span></span>
 			</a>
 		</div>
 	</div>
@@ -35,10 +31,12 @@ $inlineHTML = $fields['field_inline_html']->content;
 		<div class="tile-wrap">
 			<div class="header zeta"><?php print $headline ?></div>
 		</div>
-	
+
 		<div class="inline-content">
 			<?php print $inlineHTML ?>
 		</div>
 	</div>
 
 <?php endif ?>
+
+<img src=“image.jpg” alt=“image description” title=“image tooltip”>
