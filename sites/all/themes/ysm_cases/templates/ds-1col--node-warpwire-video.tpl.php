a4739492c676d83a2851004a64a40b7bec59ff24
Carlos Coto <cc2473@SOM-DKQR07LF9VN.som.yale.internal>
2017-07-17 16:19:32 -0400

    Added latest Warpwire Video node feature and rendering of Video Date / Location

--- sites/all/themes/ysm_cases/templates/ds-1col--node-warpwire-video.tpl.php
+++ sites/all/themes/ysm_cases/templates/ds-1col--node-warpwire-video.tpl.php
@@ -14,7 +14,28 @@
 
 		// render the media core url (the video)
 		print render($content['field_warpwire_embed']);
-		
+		// Warpwire Video Date and Location logic
+		$date = null;
+		$location = null;
+		if (array_key_exists('field_video_month' , $content)) {
+			$date = $content['field_video_month'][0]['#markup'];
+		}
+		if (array_key_exists('field_video_day' , $content)) {
+			$date .= ' ' . $content['field_video_day'][0]['#markup'];
+		}
+		if (array_key_exists('field_video_year' , $content)) {
+			$date .= ' ' .substr($content['field_video_year'][0]['#markup'] , 0 , 4);
+		}
+		if (array_key_exists('field_video_location' , $content)) {
+			$location = ' ' . $content['field_video_location'][0]['#markup'];
+		}
+		if ((isset($date)) && isset($location)) {
+			$dash = ' - ';
+		} else {
+			$dash = '';
+		}
+		print "<i> $date $dash $location </i>";
+
 	// close the div element
 	print '</div>';
 ?>
